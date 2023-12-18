0e6182e1400659fe9a9baa04554934e638d44742
Mohamed Alsaee <44772147+mohamed97alsaee@users.noreply.github.com>
2023-12-18 10:33:06 +0200

    add structure.

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:free_to_game/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(context,
+          CupertinoPageRoute(builder: (context) => const HomeScreen()));
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Splash"),
+      ),
+    );
+  }
+}
